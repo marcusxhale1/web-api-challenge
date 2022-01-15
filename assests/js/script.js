6981b8e5294b28ffac082ca22b99760b25786da1
Marcus Hale <marcusxhale1@gmail.com>
2022-01-15 14:24:32 -0800

    refactored code in JS

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,9 +1,12 @@
 var startButton = document.querySelector("#start-game");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-startButton.addEventListener("click", function(){
+var createTaskHandler = function() {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new question.";
     tasksToDoEl.appendChild(listItemEl);
-})
+}
+
+startButton.addEventListener("click", createTaskHandler);
+
