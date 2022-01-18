3ac60e5712401526e0167e7af2c9168b4574b501
Marcus Hale <marcusxhale1@gmail.com>
2022-01-17 17:22:03 -0800

    okay you can now add questions and will tell you what type of question this is as well.

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,12 +5,19 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
-    var listItemEl = document.createElement("li");
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = taskNameInput
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 
+    
 }
 
 formEl.addEventListener("submit", createTaskHandler);
