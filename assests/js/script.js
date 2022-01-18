07fc197c2c5a501cb7ef4a7fa81afc378e7608e2
Marcus Hale <marcusxhale1@gmail.com>
2022-01-17 17:01:40 -0800

    the questions can now be added

--- assests/js/script.js
+++ assests/js/script.js
@@ -4,11 +4,13 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
     var listItemEl = document.createElement("li");
+    
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new question.";
+    listItemEl.textContent = taskNameInput
     tasksToDoEl.appendChild(listItemEl);
+
 }
 
 formEl.addEventListener("submit", createTaskHandler);
