9bb1deb46fed43682471657508926cdcbf44fb7b
Marcus Hale <marcusxhale1@gmail.com>
2022-01-17 16:52:12 -0800

    this is adding drop down to the type of question this person is asking

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,12 +1,15 @@
-var startButton = document.querySelector("#start-game");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new question.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-startButton.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
